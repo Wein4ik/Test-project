c11ea217c9ee01ced1ce920599e6764f4357c021
Wein4ik <il.stafeev@yandex.ru>
2022-09-28 17:02:12 +0300

    Add hand 404 and redirect

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from women.views import *
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('women.urls')),
 ]
+
+handler404 = pageNotFound
