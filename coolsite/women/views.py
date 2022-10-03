0e7823bbafcf117b7e2aa6974d54917748c47c3b
Wein4ik <il.stafeev@yandex.ru>
2022-10-03 17:58:40 +0300

    Add model Category and rec htmls

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -6,22 +6,27 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'}
-]
+        ]
 
 
 # Create your views here.
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
+
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0
     }
     return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def show_post(request, post_id):
     return HttpResponse(f'Отображение статьи с id = {post_id}')
@@ -30,11 +35,29 @@ def show_post(request, post_id):
 def addpage(request):
     return HttpResponse('Добавление статьи')
 
+
 def contact(request):
     return HttpResponse('Обратная связь')
 
+
 def login(request):
     return HttpResponse('Авторизация')
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    if len(posts) == 0:
+        raise Http404
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def pageNotFound(request, exception):
